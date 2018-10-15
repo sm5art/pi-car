23bc3d0c5660c1c73782b72830bad1dd9becebb3
Artur K <art@network.ru>
2018-10-14 23:07:24 -0700

    noice

--- one_direction.py
+++ one_direction.py
@@ -2,11 +2,22 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
-motor_pin = 26
-GPIO.setup(motor_pin, GPIO.OUT)
+forward_pin = 26
+backward_pin = 19
+GPIO.setup(forward_pin, GPIO.OUT)
+GPIO.setup(backward_pin, GPIO.OUT)
 
 try:
     while True:
-        GPIO.output(motor_pin, GPIO.HIGH)
+        command = input().lower()
+        if command == 'w':
+            GPIO.output(forward_pin, GPIO.HIGH)
+            GPIO.output(backward_pin, GPIO.LOW)
+        elif command == 's':
+            GPIO.output(forward_pin, GPIO.LOW)
+            GPIO.output(backward_pin, GPIO.HIGH)
+        else:
+            GPIO.output(forward_pin, GPIO.LOW)
+            GPIO.output(backward_pin, GPIO.LOW)
 finally:
     GPIO.cleanup()
