a01f4395179e239079c66887cff67579795bc9e5
Artur K <art@network.ru>
2018-10-14 21:31:28 -0700

    noice

--- one_direction.py
+++ one_direction.py
@@ -6,9 +6,7 @@ motor_pin = 26
 GPIO.setup(motor_pin, GPIO.OUT)
 
 try:
-    GPIO.output(motor_pin, GPIO.HIGH)
-
-    time.sleep(1)
-    GPIO.output(motor_pin, GPIO.LOW)
+    while True:
+        GPIO.output(motor_pin, GPIO.HIGH)
 finally:
     GPIO.cleanup()
