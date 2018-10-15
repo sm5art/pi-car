beee15f7b703b14a2ed5e26dce83afc3fb00c189
Artur K <art@network.ru>
2018-10-14 23:10:06 -0700

    noice

--- one_direction.py
+++ one_direction.py
@@ -6,18 +6,25 @@ forward_pin = 26
 backward_pin = 19
 GPIO.setup(forward_pin, GPIO.OUT)
 GPIO.setup(backward_pin, GPIO.OUT)
+n_seconds = 5
 
 try:
     while True:
         command = input().lower()
         if command == 'w':
-            GPIO.output(forward_pin, GPIO.HIGH)
-            GPIO.output(backward_pin, GPIO.LOW)
+            start = time.time()
+            while(time.time() - start < n_seconds):
+                GPIO.output(forward_pin, GPIO.HIGH)
+                GPIO.output(backward_pin, GPIO.LOW)
         elif command == 's':
-            GPIO.output(forward_pin, GPIO.LOW)
-            GPIO.output(backward_pin, GPIO.HIGH)
+            start = time.time()
+            while(time.time() - start < n_seconds):
+                GPIO.output(forward_pin, GPIO.LOW)
+                GPIO.output(backward_pin, GPIO.HIGH)
         else:
-            GPIO.output(forward_pin, GPIO.LOW)
-            GPIO.output(backward_pin, GPIO.LOW)
+            start = time.time()
+            while(time.time() - start < n_seconds):
+                GPIO.output(forward_pin, GPIO.LOW)
+                GPIO.output(backward_pin, GPIO.LOW)
 finally:
     GPIO.cleanup()
